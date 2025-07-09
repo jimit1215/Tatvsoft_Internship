44e68f2910c26714bc822a43a9a6a9f145735b2b
Jimit S. Dagli <daglijimit566@gmail.com>
2025-07-09 12:43:10 +0530

    hi

---
+++ Day 4/Mission/Mission.Api/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Mission.Entities;
+using Mission.Repositories.IRepository;
+using Mission.Repositories.Repository;
+using Mission.Services.IService;
+using Mission.Services.Service;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<MissionDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "localhost",
+        ValidAudience = "localhost",
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["JwtConfig:Key"]!)
+        ),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+
+builder.Services.AddCors(cors => cors.AddPolicy("MyPolicy", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+}));
+
+
+builder.Services.AddScoped<JwtService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseCors("MyPolicy");
+
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+app.Run();
